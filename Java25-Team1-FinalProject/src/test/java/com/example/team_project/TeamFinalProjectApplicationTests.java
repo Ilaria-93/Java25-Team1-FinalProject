0e5ec13dd3053a22c4f2f12576c2de77731c1c9c
Ilaria Faleschini <150346358+Ilaria-93@users.noreply.github.com>
2024-07-16 14:40:11 +0200

    Add files via upload

---
+++ Java25-Team1-FinalProject/src/test/java/com/example/team_project/TeamFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
