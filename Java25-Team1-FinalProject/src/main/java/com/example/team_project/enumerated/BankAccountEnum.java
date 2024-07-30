880cfd6e520326b79af353c7e8d078ef821491f2
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-07-30 18:03:35 +0200

    BankAccountEnum fix

--- Java25-Team1-FinalProject/src/main/java/com/example/team_project/enumerated/BankAccountEnum.java
+++ Java25-Team1-FinalProject/src/main/java/com/example/team_project/enumerated/BankAccountEnum.java
@@ -2,19 +2,32 @@ package com.example.team_project.enumerated;
 
 public enum BankAccountEnum {
 
-    CHECKING("Checking"),
-    SAVINGS("Savings"),
-    CREDIT_CARD("Credit Card"),
-    LOAN("Loan");
+    CHECKING("Checking", 0.01, 0),
+    SAVINGS("Savings", 0.02, 0),
+    CREDIT_CARD("Credit Card", 0.0, 50000),
+    DEBIT_CARD("Debit Card", 0.0, 25000),
+    LOAN("Loan", 0.05, 0);
 
     private final String descrizione;
+    private Double interestRate;
+    private Integer creditLimit;
 
-    BankAccountEnum(String descrizione) {
+    BankAccountEnum(String descrizione, Double interestRate, Integer creditLimit) {
         this.descrizione = descrizione;
+        this.interestRate = interestRate;
+        this.creditLimit = creditLimit;
     }
 
     public String getDescrizione() {
         return descrizione;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public Integer getCreditLimit() {
+        return creditLimit;
     }
 }
 
